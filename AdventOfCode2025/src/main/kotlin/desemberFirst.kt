9fc61bc522274d3196469e15c3e150b1c5084dcf
Vetle Drage <94364488+Vetledrage@users.noreply.github.com>
2025-12-10 00:21:43 +0100

    Done part2 day1 + som improvements part1

--- AdventOfCode2025/src/main/kotlin/desemberFirst.kt
+++ AdventOfCode2025/src/main/kotlin/desemberFirst.kt
@@ -6,7 +6,7 @@ fun desemberFirst1() {
     var counter = 0
     file.forEachLine { line ->
         if (line.isNotBlank()) {
-            val direction = line[0]
+            val direction = line.first()
             val value = line.substring(1).toInt()
             sum = if (direction == 'R') {
                 (sum + value) % 100
@@ -20,6 +20,30 @@ fun desemberFirst1() {
     println(counter)
 }
 
+fun desemberFirst2() {
+    val file = File("/Users/vetledrage/Documents/AdventsKalender/AdventOfCode2025/src/main/kotlin/input.csv")
+    var pos = 50
+    var counter = 0
+    file.forEachLine { line ->
+        if (line.isNotBlank()) {
+            val direction = line.first()
+            val steps = line.drop(1).toInt()
+
+            val delta = if (direction == 'R') 1 else -1
+
+            repeat(steps) {
+                pos = (pos + delta).mod(100)
+                if (pos == 0) counter++
+            }
+        }
+    }
+
+    println(counter)
+}
+
+
+
 fun main() {
     desemberFirst1()
+    desemberFirst2()
 }
